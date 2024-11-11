190db42f8fd57c591a367bac17c489d93ba52c28
Indhiran143i <indhiran143i@gmail.com>
2024-11-11 23:18:07 +0530

    Update Footer.js

--- Kanistore/frontend/src/components/Footer.js
+++ Kanistore/frontend/src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { GiComet } from 'react-icons/gi';  // Importing comet icon
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-8">
       <div className="container mx-auto text-center">
         CCE Department
+      </div>
+      <div className="container mx-auto text-center mt-4">
+        <span className="text-xl">COMET ASSOCIATION</span>
+        <GiComet size={30} className="inline-block ml-2 text-yellow-400" />
       </div>
     </footer>
   );
