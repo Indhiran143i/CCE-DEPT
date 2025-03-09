eba8e6d30b982b27ff14e98fd8fc9a6a993847a0
Indhiran143i <indhiran143i@gmail.com>
2025-03-09 11:39:49 +0530

    Update User.js

--- Kanistore/backend/models/User.js
+++ Kanistore/backend/models/User.js
@@ -1,1 +1,15 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  category: { type: String, required: true },
+  paymentId: { type: String, required: false },
+  chestNumber: { type: Number, unique: false },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
